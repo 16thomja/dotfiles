a244073b9ddb007d2e43e8698683a335791b1cf1
16thomja <jaredcthomas@hotmail.com>
2025-06-27 15:30:34 -0400

    Add shpool config and edit fish prompt

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 Make sure curl is installed
 
-/lb means chezmoi gets installed in .local/bin
+`/lb` means chezmoi gets installed in `~/.local/bin`
+
+Edit `chezmoi.toml` with machine-specific configs after copying and before applying
 
 ```
 cd
 sh -c "$(curl -fsLS get.chezmoi.io/lb)"
 export PATH="$HOME/.local/bin:$PATH"
 chezmoi init https://github.com/16thomja/dotfiles.git
+mkdir -p ~/.config/chezmoi
+cp ~/.local/share/chezmoi/example-chezmoi.toml ~/.config/chezmoi/chezmoi.toml
 chezmoi apply
 ```
--- private_dot_config/private_fish/functions/fish_prompt.fish
+++ private_dot_config/private_fish/functions/fish_prompt.fish
@@ -1,17 +1,16 @@
 function fish_prompt --description 'Write out the prompt'
     set -l last_status $status
     set -l normal (set_color normal)
+    set -l user_color (set_color brgreen)
     set -l status_color (set_color brgreen)
     set -l cwd_color (set_color $fish_color_cwd)
     set -l vcs_color (set_color brpurple)
     set -l prompt_status ""
-    set -l user_color (set_color brgreen)
 
-    # Since we display the prompt on a new line allow the directory names to be longer.
+    # Remove directory name length limit
     set -q fish_prompt_pwd_dir_length
-    or set -lx fish_prompt_pwd_dir_length 0
 
-    # Color the prompt differently when we're root
+    # Change suffix and cwd color when root
     set -l suffix '❯'
     if functions -q fish_is_root_user; and fish_is_root_user
         if set -q fish_color_cwd_root
@@ -20,12 +19,25 @@ function fish_prompt --description 'Write out the prompt'
         set suffix '#'
     end
 
-    # Color the prompt in red on error
+    # Show red status code on error
     if test $last_status -ne 0
         set status_color (set_color $fish_color_error)
         set prompt_status $status_color "[" $last_status "]" $normal
     end
 
-    echo -s $user_color $USER $normal@$hostname ' ' $cwd_color (prompt_pwd) $vcs_color (fish_vcs_prompt) $normal ' ' $prompt_status
+    # ─── First line: user@host, cwd, VCS, status ────────────────────────
+    echo -s \
+      $user_color $USER $normal "@" $hostname ' ' \
+      $cwd_color (prompt_pwd) \
+      $vcs_color (fish_vcs_prompt) \
+      $normal ' ' $prompt_status
+
+    # ─── Second line: [shpool:<session>] ❯ ──────────────────────────────
+    if set -q SHPOOL_SESSION_NAME
+        set_color -b brgreen black
+        echo -n "[$SHPOOL_SESSION_NAME]"
+        set_color normal
+        echo -n ' '
+    end
     echo -n -s $status_color $suffix ' ' $normal
 end
